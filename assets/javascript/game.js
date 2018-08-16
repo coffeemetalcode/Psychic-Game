53d85c844c330779929aa87a9126ec6681e0fe07
dbstocker <dstocker@notesettersinc.com>
2018-08-15 21:23:08 -0400

    IDs on pertinent html <div>'s, function to grab user input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,20 +11,22 @@ let guessesLeft = 9;
 
 function randomLetter() {
   var letters = "abcdefghijklmnopqurstuvwxyz";
-  return letters.substr( Math.floor(Math.random() * 26), 1);
+  return letters.substr(Math.floor(Math.random() * 26), 1);
 }
 
 var compLett = randomLetter();
 
 // Listen for user's letter choice
 
+document.onkeyup = function(event) {
+  // Determines which key was pressed.
+  var userLett = event.key;
 
-
-var userLett = "a";
+  console.log("Your Guesses so far: " + userLett);
+};
 
 console.log("App Guessed: " + compLett);
 
 console.log("Wins: " + wins);
 console.log("Losses: " + losses);
 console.log("Guesses Left: " + guessesLeft);
-console.log("Your Guesses so far: " + userLett);
