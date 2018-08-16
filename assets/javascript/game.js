797f2eaada9635523a9c4a87a80373b698399c9f
dbstocker <dstocker@notesettersinc.com>
2018-08-15 20:55:29 -0400

    random guess workign, html wireframe done.

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// Set score variables to 0
+
+let wins = 0;
+let losses = 0;
+
+// Set guesses remaining to 9
+
+let guessesLeft = 9;
+
+// Randomly choose a letter and assign to a compLett
+
+function randomLetter() {
+  var letters = "abcdefghijklmnopqurstuvwxyz";
+  return letters.substr( Math.floor(Math.random() * 26), 1);
+}
+
+var compLett = randomLetter();
+
+// Listen for user's letter choice
+
+
+
+var userLett = "a";
+
+console.log("App Guessed: " + compLett);
+
+console.log("Wins: " + wins);
+console.log("Losses: " + losses);
+console.log("Guesses Left: " + guessesLeft);
+console.log("Your Guesses so far: " + userLett);
