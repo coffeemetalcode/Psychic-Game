694eba3e2e6d1c298cc5409ea44439072c3233ad
dbstocker <dstocker@notesettersinc.com>
2018-08-18 20:26:33 -0400

    A little more than minimal viable product

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Psychic-Game
-Guess What Letter I'm Thinking Of
+Guess What Letter I'm Thinking Of!
+
+This was inspired by Coding Boot Camp Javascript assignment and week 3 class activities. I chose to store the available choices in a string instead of an array because I think the code for choosing a random letter from the string is simpler.
+
+During development, I made heavy use of the console to check results. You can view the output of each block of code while the game is ongoing, and you can also "cheat" by viewing the computer's guess in teh console. Doing this allowed me to find and vanquish a bug where a round would end, but the computer didn't choose a new random letter.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,55 +14,58 @@ function randomLetter() {
   return letters.substr(Math.floor(Math.random() * 26), 1);
 }
 
-//This is unnecessary if I define compGuess as above
-var compGuess = randomLetter();
-// compGuess()
-console.log(compGuess);
 
 // Listen for user's letter choice
 // TODO -->-> ignore any key that isn't a single letter press
-document.onkeyup = function (event) {
 
-  // Determines which key was pressed.
-  var userGuess = event.key;
+// create a loop
+for (i = 0; i < guessesLeft; i++) {
+  // set result of randomLetter to varialble compGuess
+  var compGuess = randomLetter();
+  document.onkeyup = function (event) {
 
-  // if the user's guess matches the computer's guess, increment wins by 1 and reset game at 10 guesses
-  if (userGuess == compGuess) {
-    wins++
-    guessesLeft = 10;
-    guessed = [];
-    // compGuess();
-  }
+    // Determines which key was pressed.
+    var userGuess = event.key;
 
-  // or else decrement remaining guesses by 1
-  else {
-    guessesLeft--;
-  }
+    // if the user's guess matches the computer's guess, increment wins by 1 and reset game at 10 guesses
+    if (userGuess != compGuess) {
+      guessesLeft--;
+      // compGuess();
+      // compGuess = randomLetter();
+    }
 
-  // when guesses decreases to 0, increment losses by 1, reset the guessed array to empty, reset guesses to 10
-  if (guessesLeft == 0) {
-    losses++;
-    guessed = [];
-    guessesLeft = 10;
-  }
+    // or else decrement remaining guesses by 1, add guessed letter to guessed array
+    else {
+      wins++;
+      guessesLeft = 10;
+      guessed = [];
+    }
 
-  // if index of guessed array is greater than or equal to 0, do nothing
-  if (guessed.indexOf(userGuess) >= 0) {
+    // when guesses decreases to 0, increment losses by 1, reset the guessed array to empty, reset guesses to 10
+    if (guessesLeft == 0) {
+      losses++;
+      guessed = [];
+      guessesLeft = 10;
+    }
 
-  }
+    // if index of guessed array is greater than or equal to 0, do nothing
+    if (guessed.indexOf(userGuess) >= 0) {
+      
+    }
 
-  // or else push guessed letters into the array
-  else {
-    guessed.push(userGuess);
-    document.getElementById("guessed").innerHTML = "Your Guesses So Far: " + guessed;
-    console.log(guessed);
-  }
+    // or else push array of guessed letters into html display until number of remaining guesses is 0. then pick a new random letter and begin again.
+    else {
+      guessed.push(userGuess);
+      document.getElementById("guessed").innerHTML = "Your Guesses So Far: " + guessed;
+      console.log("guessed: " + guessed);
+      compGuess = randomLetter();
+    }
 
-  // render the results in HTML and display to the user
-  document.getElementById("wins").innerHTML = "Wins: " + wins;
-  document.getElementById("losses").innerHTML = "Losses: " + losses;
-  document.getElementById("guesses").innerHTML = "Guesses Left: " + guessesLeft;
-
-  // TODO -->-> reset the game when a user wins or runs out of guesses. Currently, the "guessed" array and counter are reset, but the computer doesn't pick a new letter.
-
-};
+    console.log("computer guess: " + compGuess);
+    // render the results in HTML and display to the user
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("guesses").innerHTML = "Guesses Left: " + guessesLeft;
+  };
+  // TODO -->-> empty the array of guesses when the player wins a round. the array currently shows the winning guess from the previous round.
+}
